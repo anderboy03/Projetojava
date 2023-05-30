10f56bece77851afc399f77df00ddb777b5fcc4c
Anderson Clayton Ribas <andersex03@gmail.com>
2023-05-30 18:39:47 -0300

    projetojava

---
+++ src/factory/ConnectionFactory.java
@@ -0,0 +1,20 @@
+
+package factory;
+// faz as importações de classes necessárias para o funcionamento do programa 
+import java.sql.Connection; 
+// conexão SQL para Java 
+import java.sql.DriverManager; 
+// driver de conexão SQL para Java 
+import java.sql.SQLException; 
+// classe para tratamento de exceções
+public class ConnectionFactory {
+     public Connection getConnection() {
+		 try {
+			return DriverManager.getConnection("jdbc:mysql://localhost/projetojava","root","");
+		 }         
+		 catch(SQLException excecao) {
+			throw new RuntimeException(excecao);
+		 }
+     }
+}
+    
